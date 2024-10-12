2df0d8e905ceddc41c1240da672cd078602dca0a
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-10-12 16:15:04 -0230

    Divides main body into parts (Layout changes)

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ function App() {
       <div className="w-64 fixed">
         <SideNav selectedIndex={(value) => console.log(value)} />
       </div>
-      <div className="ml-64">Body</div>
+      <div className="ml-64 grid grid-col-1 md:grid-cols-6">
+        <div className="md:col-span-2 bg-fuchsia-600">Control Panel</div>
+        <div className="md:col-span-3 bg-teal-900">Icon Preview</div>
+        <div className="md:col-span-1 bg-orange-700">Ads Banner</div>
+      </div>
     </div>
   );
 }
