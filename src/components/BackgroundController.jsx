5f28a0cde07421b7e3cd2ca8e3f68c5c5657c94c
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-11-07 01:06:01 -0330

    Adds background rounded, padding and color picker controls

--- src/components/BackgroundController.jsx
+++ src/components/BackgroundController.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { Slider } from "@/components/ui/slider";
+import { useEffect, useState } from "react";
+import ColorPickerController from "./ColorPickerController";
 
 function BackgroundController() {
-  return <div>BackgroundController</div>;
+  const [rounded, setRounded] = useState(0);
+  const [padding, setPadding] = useState(40);
+  const [color, setColor] = useState("#000");
+  const storageValue = JSON.parse(localStorage.getItem("value"));
+
+  useEffect(() => {
+    const updatedValue = {
+      ...storageValue,
+      bgRounded: rounded,
+      bgRotate: padding,
+      bgColor: color
+    };
+    localStorage.setItem("value", JSON.stringify(updatedValue));
+  });
+  return (
+    <div>
+      <div className="py-2">
+        <label className="p-2 flex justify-between items-center">
+          Rounded <span> {rounded} px</span>
+        </label>
+        <Slider
+          defaultValue={[0]}
+          max={512}
+          step={1}
+          onValueChange={(event) => setRounded(event[0])}
+        />
+      </div>
+      <div className="py-2">
+        <label className="p-2 flex justify-between items-center">
+          Padding <span> {padding} px</span>
+        </label>
+        <Slider
+          defaultValue={[40]}
+          max={100}
+          step={1}
+          onValueChange={(event) => setPadding(event[0])}
+        />
+      </div>
+      <div className="py-2">
+        <label className="p-2 flex justify-between items-center">
+          Icon Color
+        </label>
+        <ColorPickerController
+          hideController={false}
+          selectedColor={(color) => setColor(color)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default BackgroundController;
